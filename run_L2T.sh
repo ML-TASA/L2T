c36718171512c381ef917e7f511c7768970d8c19
ZeenSong <zeensong@qq.com>
2025-11-27 10:54:41 +0800

    update config

--- run_L2T.sh
+++ run_L2T.sh
@@ -1,12 +1,16 @@
 # Tested successfully on the hiyouga/verl:ngc-th2.6.0-cu126-vllm0.8.4-flashinfer0.2.2-cxx11abi0 image.
 # It outperforms the Qwen2 7B base model by two percentage points on the test set of GSM8K.
 set -x
-
+TRAIN_BS=1024
+PPO_BS=256
+PPO_per_GPU_BS=16
+ROLLOUT_N=5
+N_GPUS=8
 python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files=/root/data/gsm8k/train.parquet \
     data.val_files=/root/data/gsm8k/test.parquet \
-    data.train_batch_size=2 \
+    data.train_batch_size=${TRAIN_BS} \
     data.max_prompt_length=512 \
     data.max_response_length=2048 \
     data.filter_overlong_prompts=True \
@@ -15,8 +19,8 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.model.path=Qwen2.5-3B-Instruct \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.actor.ppo_mini_batch_size=1 \
-    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=2 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=${PPO_BS} \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=${PPO_per_GPU_BS} \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
@@ -33,7 +37,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.enable_chunked_prefill=False \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.45 \
     actor_rollout_ref.rollout.max_num_batched_tokens=4096 \
-    actor_rollout_ref.rollout.n=12 \
+    actor_rollout_ref.rollout.n=${ROLLOUT_N} \
     actor_rollout_ref.rollout.max_num_seqs=64 \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=2 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
@@ -43,15 +47,10 @@ python3 -m verl.trainer.main_ppo \
     trainer.project_name='verl_grpo_example_gsm8k' \
     trainer.experiment_name='qwen2_0.5b' \
     trainer.val_before_train=False \
-    trainer.n_gpus_per_node=6 \
+    trainer.n_gpus_per_node=${N_GPUS} \
     trainer.nnodes=1 \
     trainer.save_freq=20 \
     trainer.test_freq=5 \
     +actor_rollout_ref.model.override_config._attn_implementation=sdpa \
     +actor_rollout_ref.ref.override_config.attn_implementation=sdpa \
     trainer.total_epochs=5 $@
-
-
-# reward_model.enable=True \
-# reward_model.model.path=Qwen2.5-Math-PRM-7B\
-# reward_model.micro_batch_size=4 \
